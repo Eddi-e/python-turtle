fd6a48e678cc49c12aaac556fcd7606133b338e6
Eddieeeee <bridger.eddie@gmail.com>
2020-08-29 20:52:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+turttt = turtle.Turtle()
+for x in range(0,10):
+    turttt.left(36)
+    for y in range(0,2):
+        turttt.forward(70)
+        turttt.right(60)
+        turttt.forward(70)
+        turttt.right(120)
+turtle.done
